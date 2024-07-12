0a9f1937be6e78fb37bf33b020d303d597114346
Kurtis Edwards <edwards.kurtis@gmail.com>
2024-07-11 20:22:25 -0600

    chore: Refactor App component and add styling for avatar and skill list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <SkillList />
+      </div>
     </div>
   );
 }
+
+function Avatar() {
+  return (
+    <div className="avatar">
+      <img className="img" src="profile.jpg" alt="avatar" />
+    </div>
+  );
+}
+function Intro() {
+  return (
+    <div className="avatar">
+      <p>hello</p>
+    </div>
+  );
+}
+function SkillList() {
+  return (
+    <div className="avatar">
+      <p>hello</p>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
 
 export default App;
